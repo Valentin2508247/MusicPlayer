b1e4bf298fbb605d7bc2e3b41ed5d8052d9e3e98
Valentin <Valentin2508247@mail.ru>
2020-11-14 17:56:50 +0300

    Saving logs

--- public/views/Users.js
+++ public/views/Users.js
@@ -4,10 +4,23 @@ let Users = {
         <p>
             Users page
         </p>
+        <ul id="ul-users-list">
+        </ul>
         `
     },
 
     afterRender: async (id) => {
+        db.ref("users").once('value', function(snapshot) {
+            let userList = document.querySelector("#ul-users-list");
+            userList.innerHTML = "";
+            snapshot.forEach(function(childSnapshot) {
+              var userId = childSnapshot.key;
+              var userData = childSnapshot.val();
+              let li = document.createElement("li");
+              li.innerText = userData.email + "\t" + userData.role;
+              userList.appendChild(li);
+            });
+          });
     }
 }
 
